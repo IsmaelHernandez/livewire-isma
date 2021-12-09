c5683cfdc447d356d8af06261aaae200c06d0ff0
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2021-12-09 10:40:56 -0600

    estilizar-inputimg-tailiwind

--- resources/views/livewire/article-form.blade.php
+++ resources/views/livewire/article-form.blade.php
@@ -17,9 +17,18 @@
                 {{__('Some description')}}
             </x-slot>
             <x-slot name="form">
-                <div class="col-span-6 sm:col-span-4">
-                    <x-jet-label for="image" :value="__('Image')"/>
-                    <x-jet-input wire:model="image" id="image" class="mt-1 block w-full" type="file" />
+                <div class="col-span-6 sm:col-span-4 relative">
+                    @if($image)
+                        <x-jet-danger-button wire:click="$set('image')" class="absolute bottom-2 right-2">{{__('Change Image')}}</x-jet-danger-button>
+                        <img src="{{ $image->temporaryUrl() }}" class="border-2 rounded"> 
+                    @elseif($article->image)
+                        <img src="{{ Storage::disk('public')->url($article->image) }}" alt="" >
+                    @else
+                        <div class="h-32 bg-gray-50 border-2 border-dashed rounded flex items-center justify-center">
+                            <x-jet-label for="image" class="cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition" :value="__('Select Image')"/>
+                        </div>
+                    @endif
+                    <x-jet-input wire:model="image" id="image" class="hidden mt-1 block w-full" type="file" />
                     <x-jet-input-error for="image" class="mt-3" />
                 </div>
                 <div class="col-span-6 sm:col-span-4">
--- resources/views/livewire/article-show.blade.php
+++ resources/views/livewire/article-show.blade.php
@@ -1,4 +1,12 @@
 <div>
+    {{-- header --}}
+    <x-slot name="header">
+        {{-- //estilizar el titulo --}}
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{-- titulo traducible --}}
+            {{__('View Article')}}
+        </h1>
+    </x-slot>
     <div>{{$article->id}}</div>
     <h1>{{$article->title}}</h1>
     <p>{{$article->content}}</p>
